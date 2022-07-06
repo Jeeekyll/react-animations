93a1b4a80482328028ca5c07a8d867263b2fa936
EvheniiPoddubnyi <evhenii.poddubnyi@fluidweb.io>
2022-07-06 17:52:09 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./main.scss";
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 
-function App() {
+const App = () => {
+  const mainRef = useRef(null);
+
+  const circleRedRef = useRef(null);
+  const circleBlueRef = useRef(null);
+  const circleGreenRef = useRef(null);
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleExpand = () => {
+    gsap.to(circleBlueRef.current, {
+      height: 200,
+      width: 200,
+      duration: 0.4,
+      x: 60,
+      ease: "power3.easeOut",
+    });
+
+    setIsActive(true);
+  };
+
+  const handleShrink = () => {
+    gsap.to(circleBlueRef.current, {
+      height: 80,
+      width: 80,
+      duration: 0.4,
+      x: 0,
+      ease: "power3.easeOut",
+    });
+
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    gsap.to(mainRef.current, { duration: 0, css: { visibility: "visible" } });
+
+    const circles = [
+      circleRedRef.current,
+      circleBlueRef.current,
+      circleGreenRef.current,
+    ];
+
+    gsap.fromTo(
+      circles,
+      { x: -100, opacity: 0, duration: 0.8, ease: "power3.easeOut" },
+      { x: 0, opacity: 1, stagger: 0.2 }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={mainRef} className="main">
+      <div className="container">
+        <div className="main__row">
+          <div ref={circleRedRef} className="circle circle_red" />
+          <div
+            onClick={!isActive ? handleExpand : handleShrink}
+            ref={circleBlueRef}
+            className="circle circle_blue"
+          />
+          <div ref={circleGreenRef} className="circle circle_green" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
